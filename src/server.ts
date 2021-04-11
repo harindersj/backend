0f1d054611a9908264a7b3436f37733ce579b191
harindersj <harindersingh1444@gmail.com>
2021-04-11 17:36:18 +0530

    socket added

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
 import express from "express";
 import mongoose from "mongoose"
 import  {api}  from "./route/Route";
-
+import http from "http";
+import {Server} from "socket.io";
+import cors from "cors"
 // dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
-
-// app.use(helmet());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+const io= http.createServer(app);
 app.use(express.json())
+app.use(cors())
+
+io.on("connection", socket => {
+    socket.emit("your id", socket);
+    socket.on("send message", body => {
+        io.emit("message", body)
+    })
+})
+
 mongoose.connect('mongodb://localhost:27017/', {
     dbName: 'chatapp', //"mongodb+srv://harinder:uRsuoNcurgLsKHHs@cluster0.tfhsj.mongodb.net/mydb?retryWrites=true&w=majority",
    useNewUrlParser: true, useUnifiedTopology: true },
@@ -20,5 +28,6 @@ mongoose.connect('mongodb://localhost:27017/', {
     console.log("not connected")
 });
 app.use("/api", api);
+
 
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
